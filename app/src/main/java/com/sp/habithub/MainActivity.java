f3ac8bc2f31497901f0fbcab4cbd306674bb4967
staceemae77 <stacee.estrada@gmail.com>
2024-07-10 23:08:52 +0800

    first commit(main to login button working) I AM A GODDDDDD

--- app/src/main/java/com/sp/habithub/MainActivity.java
+++ app/src/main/java/com/sp/habithub/MainActivity.java
@@ -3,6 +3,9 @@ package com.sp.habithub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, activity_login.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+
     }
 }
+/*
+
+button.setOnClickListener(new View.OnClickListener() {
+@Override
+public void onClick(View v) {
+        Intent intent = new Intent(MainActivity.this, activity_login.class);
+        startActivity(intent);
+        }
+        });
+
+
+ */
